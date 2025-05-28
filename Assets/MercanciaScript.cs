ab7f34123905a849d8c1d0ffd4420660dbc7c3e9
LucasPar08 <lucaspark2008@gmail.com>
2025-05-28 07:55:50 -0300

    Destroy

---
+++ Assets/MercanciaScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MercanciaScript : MonoBehaviour
+{
+    public int scorePoint;
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name=="Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
